05b8f0ac053d89cd589e7bf9121c5a91480132ec
freesubup <freesubup@gmail.com>
2024-09-03 11:14:46 +0200

    Add files via upload

---
+++ components/empty-screen.tsx
@@ -0,0 +1,14 @@
+import { ExternalLink } from '@/components/external-link'
+
+export function EmptyScreen() {
+  return (
+    <div className="mx-auto max-w-2xl px-4">
+      <div className="flex flex-col gap-2 rounded-2xl bg-zinc-50 sm:p-8 p-4 text-sm sm:text-base">
+        <h1 className="text-2xl sm:text-3xl tracking-tight font-semibold max-w-fit inline-block">
+          Codey Chatbot
+        </h1>
+
+      </div>
+    </div>
+  )
+}
